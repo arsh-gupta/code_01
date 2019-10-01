95deb540ba224af20d6e14a8159e7289c7c6b51c
arsh-gupta <56013943+arsh-gupta@users.noreply.github.com>
2019-10-01 23:22:20 +0530

    Update fibonacci_01.py

--- fibonacci_01.py
+++ fibonacci_01.py
@@ -1,6 +1,7 @@
-n = int(input("Enter no. of Elements : "))
-f,s = 1,1
-for i in range(n):
+x = int(input("Enter no. of Elements : "))
+f=1
+s=1
+for i in range(x):
     print(f,end = ' ')
     t = f+s
     f = s
